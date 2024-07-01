1f4b98fe8d333d54cb16dc4e8b96690c2692cf1c
Cristina Acquani <cacquani@mail.com>
2024-07-01 23:02:46 +0100

    Stubbing help message

--- sources/diceware.c
+++ sources/diceware.c
@@ -2,8 +2,19 @@
 
 int main(int argc, char **argv)
 {
-  /* change */
-  printf("Hello, %s!\n", argv[1]);
+  /* First naive version of printing the help
+   * Need to figure out how to embed the text file in the compiled binary
+   * instead of reading it on the fly every time (and making the executable
+   * dependant on location)
+   * Will also need something similar for the words lists? */
+  int c;
+  FILE *file;
+  file = fopen("sources/help", "r");
+  if (file) {
+    while ((c = getc(file)) != EOF)
+      putchar(c);
+    fclose(file);
+  }
 
   return 0;
 }
