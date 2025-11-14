2ac14900958ca2ad9c9af900bfe258e202fc902b
cacquani <cacquani@mail.com>
2025-11-14 20:28:15 +0000

    Move help to a separate function

--- sources/diceware.c
+++ sources/diceware.c
@@ -1,20 +1,25 @@
 #include "includes/diceware.h"
 
-int main(int argc, char **argv)
+/* First naive version of printing the help
+ * Need to figure out how to embed the text file in the compiled binary
+ * instead of reading it on the fly every time (and making the executable
+ * dependant on location)
+ * Will also need something similar for the words lists? */
+void help()
 {
-  /* First naive version of printing the help
-   * Need to figure out how to embed the text file in the compiled binary
-   * instead of reading it on the fly every time (and making the executable
-   * dependant on location)
-   * Will also need something similar for the words lists? */
   int c;
   FILE *file;
-  file = fopen("sources/files/help", "r");
+  file = fopen("sources/help", "r");
   if (file) {
     while ((c = getc(file)) != EOF)
       putchar(c);
     fclose(file);
   }
+}
+
+int main(int argc, char **argv)
+{
+  help();
 
   return 0;
 }
--- sources/includes/diceware.h
+++ sources/includes/diceware.h
@@ -1,3 +1,5 @@
 #include <stdio.h>
 
+void help();
+
 int main(int argc, char **argv);
